59e5dcd840d73e3e58e2bd4f36ad75e3cf8bd72e
Himanshu <himanshu.tempospace@gmail.com>
2024-01-25 21:09:40 -0500

    Converted code to fun

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const img_container = document.getElementById("random_image");
-img_container.src = "https://picsum.photos/200/300";
+function changeImage() {
+    console.log("changeImage");
+    let img_container = document.getElementById("random_image");
+    if (img_container.src === "https://picsum.photos/200/300") {
+      img_container.src = "https://picsum.photos/200/300?blur";
+    } else {
+      img_container.src = "https://picsum.photos/200/300";
+    }
+  }
